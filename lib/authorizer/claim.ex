e17fe31b7209843d79e27882ea984bfc3ab8ea25
Samuel Levy <samulevy@gmail.com>
2021-08-17 21:36:01 -0300

    Remove useless files

--- lib/authorizer/claim.ex
+++ lib/authorizer/claim.ex
@@ -1,11 +1,10 @@
 defmodule Authorizer.Claim do
-  use Authorizer.Types
-
   @required [:resource_id_key, :resource_id, :roles, :permissions]
 
   @enforce_keys @required
   defstruct [:action] ++ @required
 
+  @typedoc "A struct representing the claim for an authorized action."
   @type t :: %__MODULE__{
           action: action(),
           resource_id_key: resource_id_key(),
@@ -13,4 +12,32 @@ defmodule Authorizer.Claim do
           roles: roles(),
           permissions: permissions()
         }
+
+  @typedoc """
+  The key to identify the resource that the user needs to have permission.
+  An alias for `atom()`.
+  Eg.: `:company_id`.
+  """
+  @type resource_id_key :: atom()
+
+  @typedoc "The ID of the resource that the user needs to have permission."
+  @type resource_id :: binary() | integer()
+
+  @typedoc "The user role. An alias for `atom()`."
+  @type role :: atom()
+
+  @typedoc "The name of the action to be permitted. An alias for `atom()`."
+  @type action :: atom()
+
+  @typedoc """
+  A map with a two element tuple (resource FK field and resource ID) as key
+  and a role as value.
+  """
+  @type roles :: %{{resource_id_key(), resource_id()} => role()}
+
+  @typedoc """
+  A map representing which actions the user can execute for each target resource.
+  The value is a map whith the resource field as key and a list of resource IDs as value.
+  """
+  @type permissions :: %{action() => %{resource_id_key() => [resource_id()]}}
 end
